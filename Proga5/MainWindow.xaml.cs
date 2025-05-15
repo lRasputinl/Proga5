257db747323669f2e9bc5ba94fed8e99b793aedf
lRasputinl <vlad.zaytsev.06@bk.ru>
2025-05-15 17:32:50 +0300

    Добавьте файлы проекта.

---
+++ Proga5/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Proga5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string currentFilePath = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.New, NewCommand_Executed));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Open, OpenCommand_Executed));
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Save, SaveCommand_Executed));
+        }
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextEditor.Clear();
+            currentFilePath = null;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog { Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*" };
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    TextEditor.Text = File.ReadAllText(dialog.FileName);
+                    currentFilePath = dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при открытии файла:\n{ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                var dialog = new SaveFileDialog { Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*" };
+
+                if (dialog.ShowDialog() == true)
+                    currentFilePath = dialog.FileName;
+                else
+                    return;
+            }
+
+            try
+            {
+                File.WriteAllText(currentFilePath, TextEditor.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении файла:\n{ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+}
